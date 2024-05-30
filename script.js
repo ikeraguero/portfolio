d0f3a66485dc5ec0f0b41384d4a412c7ab127920
ikeraguero <ikerpires407@gmail.com>
2024-05-30 12:21:32 -0300

    Add Animation with Observer

--- script.js
+++ script.js
@@ -33,5 +33,23 @@ const stickyNav = (entries) => {
     rootMargin: `-${navHeight}px`,
   });
   console.log(headerObserver.observe(header));
-  console.log(header);
+
+  const allSections = document.querySelectorAll('.section')
+
+  const revealSection = function(entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.remove('section--hidden');
+        observer.unobserve(entry.target);
+    });
+};
   
+  const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15
+  })
+  
+  allSections.forEach(function(section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden')
+  })
